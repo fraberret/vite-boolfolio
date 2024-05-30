6e9f7b2dcfc63c9aa349c96181ec70e831d24a6b
fraberret <f.berretta02@gmail.com>
2024-05-30 17:11:12 +0200

    save api url in a variable, import axios and call it by ajax call in mounted

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <script>
+
+import axios from 'axios';
+
 export default {
   name: 'App',
   data() {
     return {
-      projects: []
+      projects: [],
+      base_api: 'http://127.0.0.1:8000/api/projects'
     }
+  },
+  mounted() {
+    axios
+      .get(this.base_api)
+      .then(response => {
+        console.log(response);
+      })
   }
 }
 </script>
