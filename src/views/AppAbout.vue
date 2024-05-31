5c19ee6c873591ca730e69479009548c5dac0f94
fraberret <f.berretta02@gmail.com>
2024-05-31 15:36:52 +0200

    create about and contacts views

---
+++ src/views/AppAbout.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppAbout',
+}
+</script>
+
+<template>
+    <div>
+        <h1>About Page</h1>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
---
+++ src/views/AppContacts.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'AppContacts'
+}
+</script>
+
+<template>
+    <div>
+        <h1>Contacts Page</h1>
+    </div>
+</template>
+
+
+
+<style lang="scss" scoped></style>
