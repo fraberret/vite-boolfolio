9f6cd840d1a7a059502f45d34954247bf638cd6f
fraberret <f.berretta02@gmail.com>
2024-05-31 15:31:24 +0200

    create router, set the router, create home view and import the views on router

---
+++ src/router.js
@@ -0,0 +1,38 @@
+import { createWebHistory, createRouter } from 'vue-router'
+
+
+import AppHome from "./views/AppHome.vue";
+import AppBlog from "./views/AppBlog.vue";
+import AppAbout from "./views/AppAbout.vue";
+import AppContacts from "./views/AppContacts.vue";
+
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: AppHome
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: AppAbout
+        },
+        {
+            path: '/blog',
+            name: 'blog',
+            component: AppBlog
+        },
+        {
+            path: '/contacts',
+            name: 'contacts',
+            component: AppContacts
+        }
+    ]
+})
+
+
+export default router;
---
+++ src/views/AppHome.vue
@@ -0,0 +1,14 @@
+        <script>
+        export default {
+            name: 'AppHome',
+        }
+</script>
+
+<template>
+    <div>
+        <h1>HomePage</h1>
+    </div>
+</template>
+
+
+<style lang="scss" scoped></style>
