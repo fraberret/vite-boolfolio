d3cbd1f8a27a5deaa67f2385ee2c972294d75088
fraberret <f.berretta02@gmail.com>
2024-06-05 10:08:43 +0200

    add contact form

--- src/views/AppContacts.vue
+++ src/views/AppContacts.vue
@@ -5,8 +5,23 @@ export default {
 </script>
 
 <template>
-    <div>
-        <h1>Contacts Page</h1>
+    <div class="contact-us">
+        <h1>Contact Me</h1>
+        <form action="#">
+            <label for="customerName">NAME <em>&#x2a;</em></label>
+            <input id="customerName" name="customerName" required="" type="text" /><label for="customerEmail">EMAIL
+                <em>&#x2a;</em>
+            </label>
+            <input id="customerEmail" name="customerEmail" required="" type="email" />
+            <label for="customerPhone">PHONE</label>
+
+            <input id="orderNumber" name="orderNumber" type="text" />
+            <label for="customerNote">YOUR MESSAGE
+                <em>&#x2a;</em></label>
+
+            <textarea id="customerNote" name="customerNote" required="" rows="4"></textarea>
+            <button id="customerOrder">SUBMIT</button>
+        </form>
     </div>
 </template>
 
